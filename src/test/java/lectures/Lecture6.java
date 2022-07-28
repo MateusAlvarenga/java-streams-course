da90f1e1901622dee5a041662da1fc5e39f5cc12
Mateus Alvarenga <mateus_alvarenga@protonmail.com>
2022-07-28 14:34:58 -0300

    feat: lecture6

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -1,24 +1,34 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.function.Predicate;
 import org.junit.Test;
 
 public class Lecture6 {
 
-  final Predicate<Integer> numbersLessThan10 = n -> n > 5 && n < 10;
+  final Predicate<Integer> numbersBiggerThan5AndSmallerThan10 = n -> n > 5 && n < 10;
 
   @Test
   public void findAny() throws Exception {
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    final Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    final Optional<Integer> any = Arrays.stream(numbers)
+        .filter(numbersBiggerThan5AndSmallerThan10)
+        .findAny();
+    System.out.println(any.orElseThrow());
 
   }
 
   @Test
   public void findFirst() throws Exception {
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    final Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    final Optional<Integer> any = Arrays.stream(numbers)
+        .filter(numbersBiggerThan5AndSmallerThan10)
+        .findFirst();
+    System.out.println(any.orElseThrow());
 
   }
 }
