59a13a4c2dae4a1a6c534ae5d871765297b3c94f
Mateus Alvarenga <mateus_alvarenga@protonmail.com>
2022-07-28 15:42:58 -0300

    feat: lecture9

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -5,11 +5,15 @@ import com.google.common.collect.Lists;
 import java.util.Arrays;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Lecture9 {
 
   @Test
   public void reduce() throws Exception {
     Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+    final Integer sum = Arrays.stream(integers).reduce(0, Integer::sum);
+    assertThat(sum).isEqualTo(1831);
 
   }
 
