ba8b8c33c232486430f22c4c290ab66c092814e8
Mateus Alvarenga <mateus_alvarenga@protonmail.com>
2022-07-28 09:05:37 -0300

    feat: lecture3

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -12,12 +12,22 @@ public class Lecture3 {
   @Test
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    final Integer expectedMin = 1;
+    final Integer min = numbers.stream()
+        .min(Comparator.naturalOrder())
+        .orElseThrow(RuntimeException::new);
 
+    assertThat(min).isEqualTo(expectedMin);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    final Integer expectedMax = 100;
+    final Integer max = numbers.stream()
+        .max(Comparator.naturalOrder())
+        .orElseThrow(RuntimeException::new);
 
+    assertThat(max).isEqualTo(expectedMax);
   }
 }
